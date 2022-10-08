0ad46b77f88c454f77cb883181994469392e85b9
unknown <samueldavidrg152@gmail.com>
2022-10-08 11:15:36 -0600

    Modify blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,21 +1,33 @@
 from random import shuffle
 class Deck:
     def __init__(self):
-        self.get_deck()
+        self.cards = [str(i) for i in range(2, 11)] + ["A", "K", "Q", "J"]
+        self.suits = ["H", "S", "T", "D"]
+        self.deck = [card+suit for suit in self.suits for card in self.cards]
 
     def get_deck(self):
-        nums = [str(i) for i in range(2, 11)] + ["A", "K", "Q", "J"]
-        syms = ["H", "S", "T", "D"]
-        self.deck = [num+sym for sym in syms for num in nums]
+        return self.deck
 
     def shuffle_deck(self):
         shuffle(self.deck)
         print(self.deck)
+    
     def make_copy(self):
         deck_copy = self.deck.copy()
         return deck_copy
+    
+    def get_length(self):
+        print(len(self.deck))
+    
+    def deal_card(self):
+        dealt_card = self.deck.pop(0)
+        return dealt_card
 
 
 
 d = Deck()
+print(d.get_deck())
+d.get_length()
 d.shuffle_deck()
+print(d.deal_card())
+d.get_length()
