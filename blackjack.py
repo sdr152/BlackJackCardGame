14ee113055ec47fd87a7f9994b42e38694349291
unknown <samueldavidrg152@gmail.com>
2022-10-08 19:26:49 -0600

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -19,6 +19,9 @@ class Deck:
     def get_length(self):
         print(len(self.deck))
     
+    def give_first_two_cards(self):
+        return [self.deck.pop(0) for i in range(2)]
+
     def deal_card(self):
         dealt_card = self.deck.pop(0)
         return dealt_card
@@ -29,10 +32,13 @@ class RunPlayerInterface:
         self.score = 0
     
     def get_name(self):
-        raise NotImplementedError("You must implement get_name() from Player1 or Dealer")
+        raise NotImplementedError("You must implement get_name() from Player1 or Dealer.")
+
+    def get_first_hand(self):
+        raise NotImplementedError("You must implement get_first_hand() from Player1 or Dealer.")
 
     def update_hand(self):
-        raise NotImplementedError("You must implement update_hand() from Player1 or Dealer")
+        raise NotImplementedError("You must implement update_hand() from Player1 or Dealer.")
 
 class Player1(RunPlayerInterface):
     def __init__(self, name):
@@ -42,6 +48,10 @@ class Player1(RunPlayerInterface):
     
     def get_name(self):
         print(f"Player1 name: {self.name}")
+
+    def get_first_hand(self, list):
+        for card in list:
+            self.hand.append(card)
 
     def update_hand(self, card):
         self.hand.append(card)
@@ -54,6 +64,10 @@ class Dealer(RunPlayerInterface):
     
     def get_name(self):
         print(f"Dealer name: {self.name}")
+    
+    def get_first_hand(self, list):
+        for card in list:
+            self.hand.append(card)
     
     def update_hand(self, card):
         self.hand.append(card)
@@ -76,42 +90,43 @@ def add_score(card, total_score):
 
 
 def main():
-    D = Deck()
+    deck = Deck()
     player1 = Player1("Samson")
     dealer = Dealer("Beef Dealer")
+
+    first_hand1 = deck.give_first_two_cards()
+    first_hand2 = deck.give_first_two_cards()
+    
+    player1.get_first_hand(first_hand1)
+    dealer.get_first_hand(first_hand2)
     
     player1_score = 0
-    dealer_score = 0
-    n = 0
+    for cd in player1.hand:
+        v = add_score(cd, player1_score)
+        player1_score += v
+    dealer_score = add_score(first_hand2[0], 0)
     winner = None
-    while player1_score < 21 and dealer_score < 21:
-        while True:
-            x = input("Do you want to deal a new card? ")
-            if x == 'yes' or x == 'y':        
-                card = D.deal_card()
-                player1.update_hand(card)
-                player1_score += add_score(card, player1_score)
-                break
+    run = True
+    while run:
 
-        while True:
-            y = input("Do you want to deal a new card? ")
-            if y == "yes" or y == "y":        
-                card2 = D.deal_card()
-                dealer.update_hand(card2)
-                dealer_score += add_score(card2, dealer_score)
-                break
-       
-        n += 1
-
-        if n <= 1:
-            continue
         print("Player1 hand: ", player1.hand, "Score Player1: ", player1_score)
         print("Dealer hand: ", dealer.hand, "Score Dealer: ", dealer_score) 
-        if player1_score >= 21 or dealer_score >= 21:
-            break
-        ans = input("Do you wish to pull a new card?  ").lower()
-        run = ans == "y" or ans == "yes"
-    
+        
+        q = input("Do you want to deal a new card? ")
+        if q == 'no' or q == 'n':
+            run = False
+        card = deck.deal_card()
+        player1.update_hand(card)
+        player1_score += add_score(card, player1_score)
+        
+        #while True:
+        #    x = input("Do you want to deal a new card? ")
+        #    if x == 'yes' or x == 'y':        
+        #        card = deck.deal_card()
+        #        player1.update_hand(card)
+        #        player1_score += add_score(card, player1_score)
+        #    break
+ 
     winner = player1.name if player1_score > dealer_score else dealer.name
     print(f'Current winner:  {winner}') 
 
